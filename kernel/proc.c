d4ff30e5f38b72c2a2fb209563de23c27eab8312
klein <352641075@qq.com>
2024-06-04 16:58:14 +0800

    complete trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -298,6 +298,7 @@ fork(void)
 
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
+  np->tracemask=p->tracemask;
 
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
