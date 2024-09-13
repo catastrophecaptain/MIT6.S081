cf7a8eb0fd2d52afbab5ff35d4c1d043c39a5ed4
klein <352641075@qq.com>
2024-09-13 16:25:09 +0800

    fs completed

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -309,12 +309,43 @@ sys_open(void)
   struct file *f;
   struct inode *ip;
   int n;
+  int MAX_SYMLINK_DEP=11;
 
   argint(1, &omode);
   if((n = argstr(0, path, MAXPATH)) < 0)
     return -1;
 
   begin_op();
+
+  if (!(omode & O_NOFOLLOW))
+  {
+    int i = 0;
+    for (; i < MAX_SYMLINK_DEP; i++)
+    {
+      if ((ip = namei(path)) == 0)
+      {
+        break;
+      }
+      ilock(ip);
+      if (ip->type == T_SYMLINK)
+      {
+        char buf[MAXPATH];
+        readi(ip, 0, (uint64)buf, 0, MAXPATH);
+        iunlockput(ip);
+        safestrcpy(path, buf, MAXPATH);
+      }
+      else
+      {
+        iunlockput(ip);
+        break;
+      }
+    }
+    if(i == MAX_SYMLINK_DEP)
+    {
+      end_op();
+      return -1;
+    }
+  }
 
   if(omode & O_CREATE){
     ip = create(path, T_FILE, 0, 0);
@@ -334,7 +365,6 @@ sys_open(void)
       return -1;
     }
   }
-
   if(ip->type == T_DEVICE && (ip->major < 0 || ip->major >= NDEV)){
     iunlockput(ip);
     end_op();
@@ -506,5 +536,71 @@ sys_pipe(void)
 uint64
 sys_symlink(void)
 {
-    return 0;
+  char path[MAXPATH], target[MAXPATH];
+  struct inode *ip;
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+  begin_op();
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0){
+    end_op();
+    return -1;
+  }
+  // write path to ip
+  if(writei(ip, 0, (uint64)target, 0, strlen(target)) != strlen(target)){
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+  iunlockput(ip);
+  end_op();
+  return 0;
 }
+// uint64
+// sys_link(void)
+// {
+//   char name[DIRSIZ], new[MAXPATH], old[MAXPATH];
+//   struct inode *dp, *ip;
+
+//   if(argstr(0, old, MAXPATH) < 0 || argstr(1, new, MAXPATH) < 0)
+//     return -1;
+
+//   begin_op();
+//   if((ip = namei(old)) == 0){
+//     end_op();
+//     return -1;
+//   }
+
+//   ilock(ip);
+//   if(ip->type == T_DIR){
+//     iunlockput(ip);
+//     end_op();
+//     return -1;
+//   }
+
+//   ip->nlink++;
+//   iupdate(ip);
+//   iunlock(ip);
+
+//   if((dp = nameiparent(new, name)) == 0)
+//     goto bad;
+//   ilock(dp);
+//   if(dp->dev != ip->dev || dirlink(dp, name, ip->inum) < 0){
+//     iunlockput(dp);
+//     goto bad;
+//   }
+//   iunlockput(dp);
+//   iput(ip);
+
+//   end_op();
+
+//   return 0;
+
+// bad:
+//   ilock(ip);
+//   ip->nlink--;
+//   iupdate(ip);
+//   iunlockput(ip);
+//   end_op();
+//   return -1;
+// }
